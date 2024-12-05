6201d867e8e2f9a224864ed7dd2a1889c0fbb450
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2024-12-05 15:07:13 +0100

    .
    .

---
+++ script.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector('.menu-btn');
+btn.addEventListener('click', button)
+
+function changeImg() {
+    let img = document.querySelector('.img');
+    let page = (img.classList[1]);
+    const width = window.innerWidth;
+
+    if (width < 480) {
+        img.src = `../assets/img/minutos/maquettes/mobile-${page}.jpg`;
+    } else if (width >= 480 && width < 1024) {
+        img.src = `../assets/img/minutos/maquettes/tablet-${page}.jpg`;
+    } else {
+        img.src = `../assets/img/minutos/maquettes/desk-${page}.jpg`;
+    }
+}
+
+function button() {
+    let menu = document.querySelector('.menu');
+    let container = document.querySelector('.menu-container');
+
+    
+}
+
+changeImg();
+
+window.addEventListener('resize', changeImg);
